254b90d395138c14c3609e488825d0ebf39eec01
JBJA <benjamin.soleado@gmail.com>
2022-05-05 15:13:22 -0600

    05/05/2022

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Juárez Arevalo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TablasHash frm = new TablasHash();
+        frm.show();
+    }
+}
